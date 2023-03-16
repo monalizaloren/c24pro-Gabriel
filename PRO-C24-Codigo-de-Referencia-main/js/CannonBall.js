426cf3fa04778451f44fd1f4598781bd1d6e6cf3
Prof Monaliza <83433815+profmonaliza@users.noreply.github.com>
2023-03-15 22:34:33 -0300

    Update CannonBall.js

--- PRO-C24-Codigo-de-Referencia-main/js/CannonBall.js
+++ PRO-C24-Codigo-de-Referencia-main/js/CannonBall.js
@@ -1,13 +1,19 @@
-class CannonBall {
-  constructor(x, y) {
+/* Crie uma classe que se chama CannonBall 
+Com um construtor que recebe os atributos x e y*/
+//???
     var options = {
       isStatic: true
     };
-    this.r = 30;
-    this.body = Bodies.circle(x, y, this.r, options);
-    this.image = loadImage("./assets/cannonball.png");
-    this.trajectory = [];
-    World.add(world, this.body);
+    //Defina o raio da bola para 30
+    this.r = ???;
+    //Utilize o código 'Bodies.circle' para que o corpo que irá ser criado seja um círculo
+    this.body = ???(x, y, this.r, options);
+    //Utilize o código 'loadImage' para carregar uma imagem
+    this.image = ???("./assets/cannonball.png");
+    //Utilize o símbolo ' [] ', pois ele cria uma lista
+    this.trajectory = ???;
+    //Utilize o código 'World.add' para adicionar esse corpo ao mundo
+    ???(world, this.body);
   }
 
   shoot() {
@@ -16,7 +22,8 @@ class CannonBall {
     var velocity = p5.Vector.fromAngle(newAngle);
     velocity.mult(0.5);
     Matter.Body.setStatic(this.body, false);
-    Matter.Body.setVelocity(this.body, {
+    //Utilize o código ' Matter.Body.setVelocity' para aplicar uma velocidade no corpo
+    ???(this.body, {
       x: velocity.x *(180/3.14), y: velocity.y * (180/3.14)})
   }
 
@@ -28,13 +35,15 @@ class CannonBall {
     image(this.image, pos.x, pos.y, this.r, this.r);
     pop();
 
-    // Additional Activity
+    
     if (this.body.velocity.x > 0 && pos.x > 10) {
       var position = [pos.x, pos.y];
-      this.trajectory.push(position);
+      //Adicione o código 'position' para adicionar a posição da bola, dentro da lista 
+      this.trajectory.push(???);
     }
 
-    for (var i = 0; i < this.trajectory.length; i++) {
+    //Coloque que o fim do loop for será ' this.trajectory.length'
+    for (var i = 0; i < ???; i++) {
       image(this.image, this.trajectory[i][0], this.trajectory[i][1], 5, 5);
     }
   }
